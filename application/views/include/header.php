b2e4a70cce66e9c4d9ba73e4d82b65522ee0e234
Mumtaz Alam <mumtaz.alam.cs@gmail.com>
2020-07-14 20:41:11 +0500

    Updated Header and Feature Slider

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -5,8 +5,8 @@ $users = $CI->Users->profile_edit_data();
 $out_of_stock = $CI->Reports->out_of_stock_count();
 ?>
 
-<header class="section-header">
-      <section class="top-bar">
+<div class="section-head">
+      <div class="top-bar">
          <div class="container-fluid">
             <div class="row">
                <nav class="navbar navbar-expand-sm ml-md-auto">
@@ -30,85 +30,91 @@ $out_of_stock = $CI->Reports->out_of_stock_count();
                </nav>
             </div>
          </div>
-      </section>
-      <section class="header-main">
+      </div>
+      <div class="main-nav bg-light">
          <div class="container-fluid">
-            <div class="row align-items-center py-xl-1">
-               <div class="col-xl-2 col-lg-2 col-md-12">
-
-                  <div class="brand-area">
-                     <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <img src="<?php echo base_url() ?>assets/img/toggler_icon.png?>" alt="Toggle navigation">
-                     </button>
-                     <div class="company-logo">
-                        <img src="<?php echo base_url() ?>assets/img/logo.png?>" alt="Sauda Express" class="img-fluid">
-                        <p class="tag-line">Where technology meets tradition</p>   
-                     </div>
-                  </div>
-
-                  <!-- <div class="brand-area d-flex justify-content-center">
-                     <button class="navbar-toggler p-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <img src="<?php echo base_url() ?>assets/img/toggler_icon.png?>" alt="Toggle navigation">
-                     </button>
-                     <div class="company-logo mr-auto">
-                        <img src="<?php echo base_url() ?>assets/img/logo.png?>" alt="Sauda Express">
-                        <p class="tag-line">Where technology meets tradition</p>   
-                     </div>
-                  </div> -->
-
-               </div>
-               <div class="col-xl-8 col-lg-7 col-md-7 mb-sm-3 pr-sm-0">
-                  <div class="input-group mb-3 main-search-bar pl-sm-4 pl-md-0">
-                     <div class="input-group-prepend search-fields">
-                       <button class="btn btn-outline-secondary dropdown-toggle font-weight-400 category-button" type="button" data-toggle="dropdown" >
-                          All
-                          <img src="<?php echo base_url() ?>assets/img/chevron.png?>" alt="" class="img-fluid">
+            <div class="row align-items-center py-4">
+               <!-- Brand Logo & Sidebar Button -->
+               <div class="col-lg-3 col-sm-6 mb-sm-4 mb-lg-0 col-6 order-1">
+                  <div class="logo-container d-flex flex-row align-item-center justify-content-end">
+                     <div class="sidebar-button">
+                        <button class="navbar-toggler h-100" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                           <img src="<?php echo base_url() ?>assets/img/toggler_icon.png?>" alt="Toggle navigation">
                         </button>
-                       <div class="dropdown-menu">
-                         <a class="dropdown-item" href="#">Fruits</a>
-                         <a class="dropdown-item" href="#">Vegetables</a>
-                         <a class="dropdown-item" href="#">Dry Fruits</a>
-                         <a class="dropdown-item" href="#">Rice</a>
-                       </div>
                      </div>
-                     <input type="text" class="form-control font-weight-400" 
-                        placeholder="I'm shopping for...">
-                     <div class="input-group-append">
-                        <button class="btn btn-outline-secondary btn-black search-button" 
-                              type="button">Search</button>
-                      </div>
+                     <div class="logo ml-2">
+                        <div class="logo_content">
+                           <img src="<?php echo base_url() ?>assets/img/logo.png?>" alt="Sauda Express" class="img-fluid d-block">
+                           <small class="text-white tag-line">Where technology meets tradition</small> 
+                        </div>
+                     </div>
                   </div>
                </div>
-               <div class="col-xl-2 col-lg-3 col-md-5 mb-sm-2">
-                  <div class="cart-basket-main">
-                     <img src="<?php echo base_url() ?>assets/img/hotline_phone_icon.png?>" alt="" class="mt-2">
-                     <div class="hotline ml-2">
-                        <h6 class="hotline-text mb-1">Hotline</h6>
-                        <h6 class="font-weight-bold mb-1 hotline-number">+92 300 123 1234</h6>
+               <!-- Search Bar -->
+               <div class="col-lg-6 col-sm-12 order-lg-2 order-3 text-lg-left text-right">
+                  <div class="header_search">
+                     <div class="header_search_content">
+                        <div class="header_search_form_container">
+                           <form action="#">
+                              <div class="input-group mb-3">
+                                 <div class="input-group-prepend">
+                                    <button class="btn btn-outline-secondary dropdown-toggle font-weight-400 category-button" type="button"
+                                       data-toggle="dropdown">
+                                       All
+                                       <img src="<?php echo base_url() ?>assets/img/chevron.png?>" alt="" class="img-fluid">
+                                    </button>
+                                    <div class="dropdown-menu">
+                                       <a class="dropdown-item" href="#">Fruits</a>
+                                       <a class="dropdown-item" href="#">Vegetables</a>
+                                       <a class="dropdown-item" href="#">Dry Fruits</a>
+                                       <a class="dropdown-item" href="#">Rice</a>
+                                    </div>
+                                 </div>
+                                 <input type="text" class="form-control font-weight-400 border-none" placeholder="I'm shopping for...">
+                                 <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary btn-black border-none" type="button">Search</button>
+                                 </div>
+                              </div>
+                              </div>
+                           </form>
+                        </div>
                      </div>
-                     <!-- <div class="hotline-main">
-                     </div> -->
-                     <div class="cart-basket ml-0">
-                        <img src="<?php echo base_url() ?>assets/img/basket.png?>" alt="" id="basket-img">
-                        <span id="add_to_cart_items" class="badge badge-pill badge-light">2</span>
+                  </div>
+                  <!-- Phone Number & Add to Cart Button -->
+                  <div class="col-lg-3 col-6 order-lg-3 order-2 text-lg-left text-left">
+                     <div class="phone_cart mb-1 d-flex flex-row align-item-center justify-content-start">
+                        <!-- Phone -->
+                        <div class="phone mx-4 d-flex flex-row align-item-center justify-content-start">
+                           <div class="phone_icon mr-2">
+                              <img src="<?php echo base_url() ?>assets/img/hotline_phone_icon.png?>" alt="">
+                           </div>
+                           <div class="phone_content text-white">
+                              <div class="phone_text">
+                                 <h6 class="mb-0">Hotline</h6>
+                              </div>
+                              <div class="phone_number">
+                                 <h6 class="font-weight-bold">+92 300 123 1234</h6>
+                              </div>
+                           </div>
+                        </div>
+                        <!-- Cart -->
+                        <div class="cart">
+                           <div class="cart_container d-flex flex-row align-item-center justify-content-start">
+                              <!-- Cart Icon -->
+                              <div class="cart_icon">
+                                 <a href="#">
+                                    <img src="<?php echo base_url() ?>assets/img/basket.png?>" alt="" id="basket-img">
+                                    <div class="cart_icon_text">
+                                       <span id="add_to_cart_items" class="badge badge-pill badge-light">2</span>
+                                    </div>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
                      </div>
-
                   </div>
                </div>
-               <!-- <div class="col-xl-3 col-lg-3 col-md-4 mb-sm-3 cart-basket-main">
-                  <div class="float-left hotline-main">
-                     <img src="<?php echo base_url() ?>assets/img/hotline_phone_icon.png?>" alt="" class="float-left mt-2">
-                     <div class="hotline float-right ml-2">
-                        <h6 class="hotline-text mb-1">Hotline</h6>
-                        <h6 class="font-weight-bold mb-1 hotline-number">+92 300 123 1234</h6>
-                     </div>
-                  </div>
-                  <div class="cart-basket float-left">
-                     <img src="<?php echo base_url() ?>assets/img/basket.png?>" alt="" id="basket-img">
-                     <span id="add_to_cart_items" class="badge badge-pill badge-light">2</span>
-                  </div>
-               </div> -->
             </div>
          </div>
-      </section>
-   </header>
+   </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,53 +5,53 @@ $(document).ready(function(){
       infinite: false,
       speed: 300,
       responsive: [
-      {
-         breakpoint: 2559,
-         settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            infinite: true,
-            dots: true
+         {
+            breakpoint: 2559,
+            settings: {
+               slidesToShow: 5,
+               slidesToScroll: 5,
+               infinite: true,
+               dots: true
+            }
+         },
+         {
+            breakpoint: 1440,
+            settings: {
+               slidesToShow: 4,
+               slidesToScroll: 4,
+               dots: true
+            }
+         },
+         {
+            breakpoint: 1200,
+            settings: {
+               slidesToShow: 4,
+               slidesToScroll: 4,
+               dots: true
+            }
+         },
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 3
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2
+            }
+         },
+         {
+            breakpoint: 576,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               centerMode: true
+            }
          }
-      },
-      {
-         breakpoint: 1440,
-         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            dots: true
-         }
-      },
-      {
-         breakpoint: 1200,
-         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            dots: true
-         }
-      },
-      {
-         breakpoint: 992,
-         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
-         }
-      },
-      {
-         breakpoint: 768,
-         settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-         }
-      },
-      {
-         breakpoint: 576,
-         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true
-         }
-      }
       ]
    });
     
