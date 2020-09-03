c5be42fba5401310bf006d5ddb908a8ffd9fbfa2
kashif <mkmughal15@gmail.com>
2020-09-03 22:23:54 +0500

    merge master from dev for review doc1

---
+++ application/logs/log-2020-09-03.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2020-09-03 19:21:47 --> UTF-8 Support Enabled
+DEBUG - 2020-09-03 19:21:47 --> No URI present. Default controller set.
+DEBUG - 2020-09-03 19:21:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2020-09-03 19:21:48 --> Lcategory class already loaded. Second attempt ignored.
+DEBUG - 2020-09-03 19:21:48 --> Total execution time: 1.5779
+DEBUG - 2020-09-03 19:21:49 --> UTF-8 Support Enabled
+DEBUG - 2020-09-03 19:21:49 --> No URI present. Default controller set.
+DEBUG - 2020-09-03 19:21:49 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2020-09-03 19:21:49 --> Lcategory class already loaded. Second attempt ignored.
+DEBUG - 2020-09-03 19:21:49 --> Total execution time: 0.1614
